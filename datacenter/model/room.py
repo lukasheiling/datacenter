97f2ae4badb93aeb2f86509cd9d8caea799fdc96
Posch Simon <posch190076@sr.htlweiz.at>
2022-12-16 11:07:10 +0100

    room class

--- datacenter/model/room.py
+++ datacenter/model/room.py
@@ -5,6 +5,10 @@ from .floor import Floor
 class Room():
     __tablename__ = "Room"
     room_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    room_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    room_name = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=False)
     floor_id = sqlalchemy.Column(sqlalchemy.Integer, foreign_key=True)
+
+    def __repr__(self):
+        """Generate nice representation!"""
+        return "FooBar:%s" % self.room_name
 
