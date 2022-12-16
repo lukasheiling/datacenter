4e9d6a385d15137f934cdc6b8b54bef9f6df2c0f
Simon Puntigam <puntig190080@sr.htlweiz.at>
2022-12-16 11:12:19 +0100

    Socket Database

---
+++ datacenter/model/socket.py
@@ -0,0 +1,16 @@
+"""Socket Database."""
+
+from base import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+
+class Socket(Base):
+    """Socket Database."""
+
+    tablename = 'sockets'
+
+    id = Column(Integer, primary_key=True)
+    socket_name = Column(String)
+    first_connection_id = Column(Integer, ForeignKey('connections.id'))
+    second_connection_id = Column(Integer, ForeignKey('connections.id'))
+    room_id = Column(Integer, ForeignKey('rooms.id'))
