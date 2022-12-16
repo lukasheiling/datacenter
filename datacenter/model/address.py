72a0b193f60e69bc6ad628bd3f06bfed308d4145
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2022-12-16 11:45:54 +0100

    created test_01_address.py for testing the Address class

--- datacenter/model/address.py
+++ datacenter/model/address.py
@@ -1,15 +1,17 @@
-import sqlalchemy
+from sqlalchemy import Integer, ForeignKey, Column, VARCHAR, ForeignKey
+from sqlalchemy.orm import relationship
 from .base import Base
 
 class Address(Base):
     """Create the Address table"""
     __tablename__ = "Address"
-    Id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    UserId = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    City = sqlalchemy.Column(sqlalchemy.VARCHAR(255), nullable=False)
-    Street = sqlalchemy.Column(sqlalchemy.VARCHAR(255), nullable=False)
-    Housenumber = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    Zipcode = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    Id = Column(Integer, primary_key=True)
+    UserId = Column(Integer, ForeignKey('user.id'), nullable=False)
+    City = Column(VARCHAR(255), nullable=False)
+    Street = Column(VARCHAR(255), nullable=False)
+    Housenumber = Column(Integer, nullable=False)
+    Zipcode = Column(Integer, nullable=False)
+    user = relationship("User", back_populates="Address")
 
     def __repr__(self):
         """Generate nice representation!"""
