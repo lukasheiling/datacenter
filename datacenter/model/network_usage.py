92e69241eb3020c88ae7fe51235ca2bf19e9a19e
shala190035 <55692386+shala190035@users.noreply.github.com>
2022-12-16 11:35:44 +0100

    NetworkUsage update

--- datacenter/model/network_usage.py
+++ datacenter/model/network_usage.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-import sqlalchemy 
-from .base import Base
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from base import Base
 
 class NetworkUsage(Base):
     __tablename__ = 'network_usage'
 
-    network_usage_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    timestamp = sqlalchemy.Column(DateTime, default=datetime.utcnow)
-    traffic = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
-    performance = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    network_usage_id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    traffic = Column(Integer, nullable=False)
+    performance = Column(Integer, nullable=False)
     connection_id = Column(Integer, ForeignKey('connection.id'))
 
     def __repr__(self):
